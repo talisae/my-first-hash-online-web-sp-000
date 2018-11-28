eab6753ea1a38032f60094f5ac6322852b27ca3e
Talisa Erazo <talisaerazo@gmail.com>
2018-11-28 01:11:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,19 +14,11 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
   shipping_manifest["muskets"] = 2
 
-
-  # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
 
-
-  # return the shipping_manifest hash below
   shipping_manifest
 end
